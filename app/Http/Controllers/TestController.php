8d03ad72b175e0e17376b98151159148f02e8351
Kevin(Zhihao Yang) <zhihao.y@facedrive.com>
2022-08-20 14:42:20 -0400

    initial commit

---
+++ app/Http/Controllers/TestController.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Http\Controllers;
+//use App\Http\Controllers\Controller;
+
+class TestController extends Controller
+{
+    public function test(): string
+    {
+
+
+
+        return 'hello';
+    }
+
+}
