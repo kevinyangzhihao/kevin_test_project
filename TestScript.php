e6a65d8d0143c6adb2c7e003de54c421f7da835b
Kevin(Zhihao Yang) <zhihao.y@facedrive.com>
2022-09-07 18:51:50 -0400

    practice filament

--- TestScript.php
+++ TestScript.php
@@ -1,18 +1,6 @@
 <?php declare(strict_types=1);
-class myclass{
-    private $fname="Kiran";
-    public $mname="Pratap";
-    static $lname="Singh";
-    function dispdata(): void {
-        echo "$this->fname\n";
-        echo "$this->mname\n";
-        echo myclass::$lname;
-    }
-}
-$obj=new myclass();
-$obj->dispdata();
-echo $obj::$lname;
-echo $obj->mname;
 
-
-
+$my_array = array_merge(['aa'=>'bb','cc'=>'dd'],['ee'=>'ff','gg'=>'hh']);
+//$my_array = array_merge($my_array,['ee1'=>'ff1','gg1'=>'hh1']);
+$my_array[] = ['ee1'=>'ff1','gg1'=>'hh1'];
+var_dump($my_array);
